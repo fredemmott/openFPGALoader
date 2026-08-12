e343ffc3ad84cccecdde3a72143276026f193242
Fred Emmott <git@fredemmott.com>
2026-08-12 10:38:06 -0500

    Portable `isatty()` check in progressBar
    - POSIX, Linux: returns 0 or 1
    - MSDN `_isatty()` (isatty is macro for that): returns 0 or non-zero. In Windows Terminal, it's returning 64 for me
    
    Other calls are only checking truthiness, not explicitly for 1
    
    refs #704

--- src/progressBar.cpp
+++ src/progressBar.cpp
@@ -14,7 +14,7 @@
 ProgressBar::ProgressBar(const std::string &mess, int maxValue,
 		int progressLen, bool quiet): _mess(mess), _maxValue(maxValue),
 		_progressLen(progressLen), last_time(std::chrono::system_clock::now()),
-		_quiet(quiet), _first(true), _is_tty(isatty(STDOUT_FILENO) == 1)
+		_quiet(quiet), _first(true), _is_tty(isatty(STDOUT_FILENO))
 {
 }
 
